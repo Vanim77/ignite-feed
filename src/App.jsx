a8762a69237d4e8cd8180d88c7853a4c477cdc39
vanim <vanimouzad123@gmail.com>
2023-01-19 12:01:26 -0300

    :sparkles: feat: Sidebar added on App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+import styles from './App.module.css';
 import './global.css';
 
 export function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>post</main>
+      </div>
     </div>
-  )
+  );
 }
